67e8c0d6f2b6a7bb0a2796349071cfe4c2063020
Oleg Diachenko <diachenko.oleg@proton.me>
2023-12-21 22:32:37 +0300

    it is working. Do not forget to connect me via linkedin: oleg-diachenk0

---
+++ Program.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.IO;
+using System.Diagnostics;
+
+
+class DataItem
+{
+    public int Id { get; set; }
+    public double A { get; set; }
+    public double B { get; set; }
+    public double C { get; set; }
+    public double Discriminant { get; set; }
+    public bool IsDiscriminantPositive { get; set; }
+    public double SquareRootDiscriminant { get; set; }
+    public string? X1 { get; set; } = "не определено";
+    public string? X2 { get; set; } = "не определено";
+}
+
+class Program
+{
+    static Dictionary<int, DataItem> objectsById = new Dictionary<int, DataItem>();
+    static Queue<int>[] queues = new Queue<int>[6];
+    static double[] delays = { 3.0, 1.5, 3.5, 3.1, 3.8, 3.2 };
+    static int lastId = 0; // Инициализация lastId
+    static DateTime startTime; // Переменная для хранения времени начала работы
+
+    static AutoResetEvent[] autoResetEvents = new AutoResetEvent[6];
+
+    static CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(60000); // 60 секунд
+
+
+static void Main(string[] args)
+{
+    var token = cancellationTokenSource.Token;
+
+    for (int i = 0; i < autoResetEvents.Length; i++)
+    {
+        autoResetEvents[i] = new AutoResetEvent(false);
+    }
+
+    // Инициализация массива queues
+    for (int i = 0; i < queues.Length; i++)
+    {
+        queues[i] = new Queue<int>();
+    }
+
+    startTime = DateTime.Now;
+    
+    // Запуск потоков
+    var thread1 = new Thread(() => ProcessStage1(token));
+    var thread2 = new Thread(() => ProcessStage2(token));
+    var thread3 = new Thread(() => ProcessStage3(token));
+    var thread4 = new Thread(() => ProcessStage4(token));
+    var thread5 = new Thread(() => ProcessStage5(token));
+    var thread6 = new Thread(() => ProcessStage6(token));
+
+    thread1.Start();
+    thread2.Start();
+    thread3.Start();
+    thread4.Start();
+    thread5.Start();
+    thread6.Start();
+
+    var stopwatch = new Stopwatch();
+    stopwatch.Start();
+
+    // while (stopwatch.Elapsed < TimeSpan.FromSeconds(15))
+    // {
+    //     Thread.Sleep(10000); // Проверка каждую секунду
+
+    //     if (queues.All(q => q.Count == 0))
+    //     {
+    //         cancellationTokenSource.Cancel(); // Отмена, если все очереди пусты
+    //         break;
+    //     }
+    // }
+
+    thread1.Join();
+    thread2.Join();
+    thread3.Join();
+    thread4.Join();
+    thread5.Join();
+    thread6.Join();
+}
+
+    static void ProcessStage1(CancellationToken token)
+    {
+        Console.WriteLine($"{DateTime.Now - startTime} — Запущен поток 1");
+
+        string filePath = "input.txt"; // Путь к файлу с данными
+
+            try
+            {
+            // Проверка наличия файла
+                if (!File.Exists(filePath))
+                {
+                    throw new FileNotFoundException($"Файл '{filePath}' не найден");
+                }
+                else
+                {
+                    Console.WriteLine($"{DateTime.Now - startTime} — Файл существует");
+                }                
+                string[] lines;
+
+                lines = File.ReadAllLines(filePath); // Чтение всех строк файла
+
+                Console.WriteLine($"{DateTime.Now - startTime} — Прочитали файл");
+                foreach (var line in lines)
+                {
+                    var parts = line.Split(',');
+                    var dataItem = new DataItem
+                    {
+                        Id = Interlocked.Increment(ref lastId),
+                        A = double.Parse(parts[0].Trim()),
+                        B = double.Parse(parts[1].Trim()),
+                        C = double.Parse(parts[2].Trim())
+                    };
+                    Thread.Sleep(TimeSpan.FromSeconds(delays[0]));
+                    Console.WriteLine($"{DateTime.Now - startTime} — Распаковали очередную строку");
+
+                    objectsById[dataItem.Id] = dataItem;
+                    Console.WriteLine($"{DateTime.Now - startTime} — Поток 1 добавляет ID {dataItem.Id} в очередь");
+                    queues[1].Enqueue(dataItem.Id); // Добавление ID в очередь следующего потока
+                    autoResetEvents[1].Set(); // Уведомление второго потока
+
+                }
+                Console.WriteLine($"{DateTime.Now - startTime} — Обработка файла завершена, ожидаем сигнала отмены");
+                while (!token.IsCancellationRequested)
+                {
+                    Thread.Sleep(1000); // Ожидание в течение короткого времени перед повторной проверкой
+                }
+
+    Console.WriteLine($"{DateTime.Now - startTime} — Поток 1 получил сигнал отмены");
+            }
+            catch (Exception ex)
+    {
+        Console.WriteLine($"Ошибка при чтении файла: {ex.Message}");
+    }
+}
+
+static void ProcessStage2(CancellationToken token)
+{
+    while (true)
+    {
+        // Проверка, был ли сделан запрос на отмену
+        if (token.IsCancellationRequested) {
+            Console.WriteLine($"{DateTime.Now - startTime} — Поток 2 прерван");
+            return; // Завершение работы потока
+        }
+        int dataItemId = -1;
+
+            if (queues[1].Count > 0)
+            {
+                dataItemId = queues[1].Dequeue();
+                Console.WriteLine($"{DateTime.Now - startTime} — Поток 2 получил ID {dataItemId}");
+
+            }
+            else
+            {
+                autoResetEvents[1].WaitOne(TimeSpan.FromSeconds(10));
+                continue;
+            }
+
+        DataItem dataItem;
+
+        if (!objectsById.TryGetValue(dataItemId, out dataItem) || dataItem == null)
+            continue; // Пропустить итерацию, если объект не найден или равен null
+
+        dataItem.Discriminant = dataItem.B * dataItem.B - 4 * dataItem.A * dataItem.C;
+
+        // Передача id в очередь следующего потока
+
+            queues[2].Enqueue(dataItemId);
+            autoResetEvents[2].Set();
+    }
+}
+
+static void ProcessStage3(CancellationToken token)
+{
+    while (true)
+    {
+        // Проверка, был ли сделан запрос на отмену
+        if (token.IsCancellationRequested) {
+            Console.WriteLine($"{DateTime.Now - startTime} — Поток 3 прерван");
+            return; // Завершение работы потока
+        }
+        int dataItemId = -1;
+
+            if (queues[2].Count > 0)
+            {
+                dataItemId = queues[2].Dequeue();
+                Console.WriteLine($"{DateTime.Now - startTime} — Поток 3 получил ID {dataItemId}");
+
+            }
+            else
+            {
+                // Если очередь пуста, поток переходит в режим ожидания
+                autoResetEvents[2].WaitOne(TimeSpan.FromSeconds(10));
+                continue;
+            }
+
+        // Задержка потока
+        Thread.Sleep(TimeSpan.FromSeconds(delays[2]));
+
+        // Проверка значения дискриминанта и установка флага is_D_positive
+        DataItem dataItem;
+            if (!objectsById.TryGetValue(dataItemId, out dataItem))
+                continue; // Если объект не найден, пропустить итерацию
+
+            dataItem.IsDiscriminantPositive = dataItem.Discriminant >= 0;
+
+        // Передача id в очередь следующего потока
+            queues[3].Enqueue(dataItemId);
+            autoResetEvents[3].Set(); // Уведомление следующего потока
+    }
+}
+
+static void ProcessStage4(CancellationToken token)
+{
+    while (true)
+    {
+        // Проверка, был ли сделан запрос на отмену
+        if (token.IsCancellationRequested) {
+            Console.WriteLine($"{DateTime.Now - startTime} — Поток 4 прерван");
+            return; // Завершение работы потока
+        }
+        int dataItemId = -1;
+
+            if (queues[3].Count > 0)
+            {
+                dataItemId = queues[3].Dequeue();
+                Console.WriteLine($"{DateTime.Now - startTime} — Поток 4 получил ID {dataItemId}");
+
+            }
+            else
+            {
+                autoResetEvents[3].WaitOne(TimeSpan.FromSeconds(10)); // Переход в режим ожидания
+                continue;
+            }
+
+        // Задержка потока
+        Thread.Sleep(TimeSpan.FromSeconds(delays[3]));
+
+        DataItem dataItem;
+            if (!objectsById.TryGetValue(dataItemId, out dataItem))
+                continue; // Если объект не найден, пропустить итерацию
+
+            if (!dataItem.IsDiscriminantPositive)
+            {
+                dataItem.X1 = dataItem.X2 = "нет корней";
+            }
+            else
+            {
+                dataItem.SquareRootDiscriminant = Math.Sqrt(dataItem.Discriminant);
+            }
+
+        // Передача id в очередь следующего потока
+            queues[4].Enqueue(dataItemId);
+            autoResetEvents[4].Set(); // Уведомление следующего потока
+    }
+}
+
+static void ProcessStage5(CancellationToken token)
+{
+    while (true)
+    {
+        // Проверка, был ли сделан запрос на отмену
+        if (token.IsCancellationRequested) {
+            Console.WriteLine($"{DateTime.Now - startTime} — Поток 5 прерван");
+            return; // Завершение работы потока
+        }
+        int dataItemId = -1;
+            if (queues[4].Count > 0)
+            {
+                dataItemId = queues[4].Dequeue();
+                Console.WriteLine($"{DateTime.Now - startTime} — Поток 5 получил ID {dataItemId}");
+
+            }
+            else
+            {
+                autoResetEvents[4].WaitOne(TimeSpan.FromSeconds(10)); // Переход в режим ожидания
+                continue;
+            }
+
+        // Задержка потока
+        Thread.Sleep(TimeSpan.FromSeconds(delays[4]));
+
+        DataItem dataItem;
+            if (!objectsById.TryGetValue(dataItemId, out dataItem))
+                continue; // Если объект не найден, пропустить итерацию
+
+            if (dataItem.IsDiscriminantPositive)
+            {
+                double a = dataItem.A, b = dataItem.B, d = dataItem.SquareRootDiscriminant;
+                dataItem.X1 = ((-b + d) / (2 * a)).ToString();
+                dataItem.X2 = ((-b - d) / (2 * a)).ToString();
+            }
+
+        // Передача id в очередь следующего потока
+            queues[5].Enqueue(dataItemId);
+            autoResetEvents[5].Set(); // Уведомление следующего потока
+    }
+}
+
+static void ProcessStage6(CancellationToken token)
+{
+    while (true)
+    {
+        // Проверка, был ли сделан запрос на отмену
+        if (token.IsCancellationRequested) {
+            Console.WriteLine($"{DateTime.Now - startTime} — Поток 6 прерван");
+            return; // Завершение работы потока
+        }
+        int dataItemId = -1;
+
+            if (queues[5].Count > 0)
+            {
+                dataItemId = queues[5].Dequeue();
+                Console.WriteLine($"{DateTime.Now - startTime} — Поток 6 получил ID {dataItemId}");
+
+            }
+            else
+            {
+                autoResetEvents[5].WaitOne(TimeSpan.FromSeconds(10)); // Переход в режим ожидания
+                continue;
+            }
+
+        // Задержка потока
+        Thread.Sleep(TimeSpan.FromSeconds(delays[5]));
+
+        DataItem dataItem;
+            if (objectsById.TryGetValue(dataItemId, out dataItem))
+            {
+                // Запись данных в файл
+                WriteResultsToFile(dataItem);
+                Console.WriteLine($"{DateTime.Now - startTime} — Поток 6 записал результат в словарь ID {dataItemId}");
+                // Удаление объекта из словаря
+                objectsById.Remove(dataItemId);
+            }
+    }
+}
+
+static void WriteResultsToFile(DataItem dataItem)
+{
+    string resultLine = $"ID: {dataItem.Id}, A: {dataItem.A}, B: {dataItem.B}, C: {dataItem.C}, D: {dataItem.Discriminant}, X1: {dataItem.X1}, X2: {dataItem.X2}";
+        File.AppendAllText("result.txt", resultLine + Environment.NewLine);
+}
+}
--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # quadratic-equation
 The client asked for a simple implementation of finding the roots of a quadratic equation in C#. Paid in currency for each line of code. So I didn't hold back. 😏😜🤑
+
+## Chapter 1
+The application simulates conveyor-belt data processing, where each data object from the input file sequentially goes through a series of processes performed by various threads. The application consists of six threads, each responsible for a specific stage of data processing. The threads operate asynchronously and use accumulators in queue mode to store data objects awaiting processing.
+
+As soon as data enters the queue of the first thread, it gets activated, creates objects from the read data, and places them in a global dictionary for access. Each object receives a unique identifier, which is used for transferring between threads. Then this identifier is passed to the queue of the next thread, which, after a delay, starts its part of the processing.
+
+The threads perform different operations, including calculating the discriminant, checking its value, computing the roots of the equation, and deciding on the format for recording results. If at any stage there is no data to process, the corresponding thread enters a waiting mode. After processing is completed by each thread, the final thread writes the results to an output file and removes the processed data from the global dictionary.
+
+Thus, the application ensures continuous and efficient data processing, where each thread specializes in a certain type of operations and works in coordination with other threads to ensure a smooth and sequential flow of data through the conveyor.
+
+
+## Chapter 2
+
+In our work, we faced a choice between using the interrupt method and signals like AutoResetEvent for synchronization and thread management in a multi-threaded C# application. After thorough analysis, we concluded that signals are a more preferable option for our purposes.
+
+The interrupt method offers a direct way to interrupt a thread in a waiting, blocking, or sleeping state. However, its use can lead to a ThreadInterruptedException, making it less ideal for complex multi-threaded applications that require precise understanding and control of thread states. Additionally, it has limited flexibility as it only affects threads in certain states.
+
+On the other hand, AutoResetEvent and similar signals provide a high level of abstraction and control, allowing one thread to notify another about the occurrence of specific events. This is particularly useful in scenarios where fine coordination between threads is needed. Such an approach offers better coordination and flexibility, especially when complex synchronization logic and interaction between different data processing stages are required.
+
+In the context of our project, which requires reliable interaction between threads for processing and transferring data through various stages, using AutoResetEvent seems to be a more sensible choice. It not only simplifies thread state management but also reduces the risk of errors associated with thread interruption.
+
+Regarding the question of adding Thread.Abort or Thread.ResetAbort at the end of the code, it’s worth noting that Thread.Abort is considered an outdated and potentially dangerous practice in modern C# programming. Instead, safer and more manageable methods like CancellationToken are recommended for stopping threads. This helps avoid issues related to the safety and stability of the application's execution.
+
+
+## Chapter 3
+
+In our work, we also considered the use of CancellationToken and Thread.Abort for managing threads in our multi-threaded C# application. After analyzing various approaches, we concluded that CancellationToken is a more preferable and safe solution.
+
+Thread.Abort is a method used for the immediate interruption of a thread. However, its use can be dangerous as it leads to the generation of a ThreadAbortException, potentially interrupting the thread execution at an unpredictable point. This can cause resource leaks, unfinished I/O operations, and other stability issues. Due to these risks, Thread.Abort is considered obsolete and not recommended for use in modern applications.
+
+On the other hand, CancellationToken represents a mechanism for cooperative cancellation of operations, making it safer and more flexible. CancellationToken allows threads to check for a cancellation request at convenient points and properly conclude their work, freeing up resources and completing necessary operations. This ensures a more predictable and controlled termination of threads.
+
+In the context of our project, using CancellationToken provides effective thread management, allowing us to correctly terminate operations when necessary, without the risk of sudden exceptions or system instability. We can pass the cancellation token to each thread and regularly check its state within the threads, which gives us control and flexibility in managing multi-threaded processing.
+
+Thus, opting for CancellationToken over Thread.Abort allows us to ensure more reliable and stable execution of our application, minimizing risks associated with forced thread interruption.
+
+Additionally, in our analysis of using CancellationToken and Thread.Abort in a multi-threaded C# application, we also considered the possibility of using methods like Thread.Suspend() and Thread.Resume(). However, we concluded that these methods also are not suitable for our project for several reasons:
+
+Unpredictability and Risk of Deadlocks: Thread.Suspend() suspends a thread at an arbitrary execution point, which can lead to deadlocks if the thread was stopped in a critical segment of code, for example, while holding a lock. This makes the use of Suspend() and Resume() risky and unpredictable in multi-threaded applications.
+
+Obsolescence of Methods: Similar to Thread.Abort, methods like Thread.Suspend() and Thread.Resume() are considered outdated in modern .NET due to their unreliability and security issues. The use of these methods is not recommended, and they may be completely removed from future versions of the .NET platform.
+
+Lack of Control: These methods do not provide threads the opportunity to properly handle their state before suspension or after resumption. Unlike CancellationToken, which allows threads to cooperatively and safely respond to cancellation requests, Suspend and Resume interrupt thread execution without offering an opportunity for proper handling of the current state.
+
+Complexities in Debugging and Testing: Using Thread.Suspend() and Thread.Resume() can significantly complicate the process of debugging and testing an application, as threads can be suspended at arbitrary points, making it difficult to determine the state of the program and the sources of errors.
+
+Based on these considerations, we decided not to use Thread.Suspend() and Thread.Resume() in our application, and instead rely on safer and more controlled thread management mechanisms like CancellationToken, which provide a more stable and predictable multi-threaded processing.
